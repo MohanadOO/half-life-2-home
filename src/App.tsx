e523d6a1fcad2b34f239db736b8c59ec51c414a8
MohanadOO <moohand220@gmail.com>
2022-07-30 22:53:28 +0300

    Remove background noise ❌

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,6 @@ import AboutDeveloper from './components/AboutDeveloper'
 function App() {
   return (
     <>
-      <div className='after:fixed after:left-0 after:top-0 after:w-screen after:h-screen after:opacity-[0.03] after:pointer-events-none after:bg-[url(/noise.gif)] z-50'></div>
       <Nav />
       <Hero />
       <div className='my-5 mx-5 sm:mx-10 md:mx-14 lg:mx-20 flex flex-col child:my-7 child:lg:my-16 child:xl:my-20 child:scroll-mt-32'>
